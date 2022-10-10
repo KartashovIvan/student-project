cf433cb13d632c524b34e6d825ceedf7b9a2e717
KartashovIvan <vano.noname69@yandex.ru>
2022-10-10 22:12:41 +0300

    Lesson 10

--- src/WeddingValidator.java
+++
@@ -1,6 +0,0 @@
-public class WeddingValidator {
-    static AnswerWedding checkWedding(StudentOrder so){
-        System.out.println("checkWedding is running");
-        return new AnswerWedding();
-    }
-}
