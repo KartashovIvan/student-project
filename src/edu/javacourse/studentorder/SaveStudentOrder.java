466803bbae70615ea16168e34b7fd07854e2e79c
KartashovIvan <vano.noname69@yandex.ru>
2022-10-11 19:17:56 +0300

    Lesson 12

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -1,23 +1,27 @@
 package edu.javacourse.studentorder;
 
+import edu.javacourse.studentorder.domain.Adult;
 import edu.javacourse.studentorder.domain.StudentOrder;
 
 public class SaveStudentOrder {
     public static void main(String[] args) {
         StudentOrder so = new StudentOrder();
-        so.sethFirstName("Алексей");
-        so.sethLastName("Петров");
-        so.setwFirstName("Галина");
-        so.setwLastName("Петрова");
-
         long ans = saveStudentOrder(so);
         System.out.println(ans);
     }
 
     static long saveStudentOrder(StudentOrder studentOrder){
         long answer = 199;
-        System.out.println("saveStudentOrder:" + studentOrder.gethLastName());
+        System.out.println("saveStudentOrder:");
 
         return answer;
+    }
+
+    static StudentOrder buildStudentOrder(){
+        StudentOrder so = new StudentOrder();
+        Adult husband = new Adult();
+        so.setHusband(husband);
+
+        return so;
     }
 }
